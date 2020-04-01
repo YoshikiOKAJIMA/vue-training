bffd47a29a823b843682f54b0c9556dc98e302bd
YoshikiOKAJIMA <ihsoy.0315@gmail.com>
2020-04-01 20:26:01 +0900

    Some modification
    To make it work properly, dependency of "axios" was reduced.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,77 @@
 new Vue({
     el: '#app',
     data: {
-        output: '0',
         nextBtn: "次の問題へ",
-        qNumList: [99999, 99999, 99999],
+        qNumList: [9, 8, 7],
         message: '',
         count: 0,
         countCorrect: 0,
         result: false,
         btnActive: true,
-        qList: []
+        qList: [
+            {
+                id: 1,
+                question: "abcde",
+                option: [{
+                        index: 1,
+                        sentence: "aaaaa",
+                        correct: true
+                },
+                    {
+                        index: 2,
+                        sentence: "bbbbb",
+                        correct: false
+                },
+                    {
+                        index: 3,
+                        sentence: "ccccc",
+                        correct: false
+                }]
+            },
+            {
+                id: 2,
+                question: "fghij",
+                option: [{
+                        index: 1,
+                        sentence: "ddddd",
+                        correct: false
+                },
+                    {
+                        index: 2,
+                        sentence: "eeeee",
+                        correct: false
+                },
+                    {
+                        index: 3,
+                        sentence: "fffff",
+                        correct: true
+                }]
+            },
+            {
+                id: 3,
+                question: "klmno",
+                option: [{
+                        index: 1,
+                        sentence: "ggggg",
+                        correct: true
+                },
+                    {
+                        index: 2,
+                        sentence: "hhhhh",
+                        correct: false
+                },
+                    {
+                        index: 3,
+                        sentence: "iiiii",
+                        correct: false
+                }]
+            }
+
+    ],
+        loop: 0,
+        title: 'QUIZ GAME'
     },
     methods: {
-        pressKey: function (value) {
-            this.output += value
-        },
         //解答の正誤判定+解答ボタンの不活化
         judgeCorrect: function (c) {
             if (this.btnActive) {
@@ -43,22 +100,33 @@ new Vue({
         }
     },
     created: function () {
-        axios.get('data/quiz.json').then(function (response) {
+        //追加の出題データ※機能しない！原因不明。
+        axios.get('quiz.json').then(function (response) {
             //取得完了したらqListリストに代入
-            this.qList = response.data
+            this.qList.push(response.data);
+            console.log(this.qList.length)
+        }.bind(this)).catch(function (e) {
+            console.error(e)
         })
 
         //出題する問題を3つ、ランダムに決定
+        var a = 0;
         for (var i = 0; i < this.qNumList.length; i++) {
-            var a = Math.floor(Math.random() * this.qList.length)
-            //問題の重複回避
-            if (this.qNumList.includes(a)) {
-                i -= 1
+            a = Math.floor(Math.random() * this.qList.length)
+            this.loop++
+            if (this.loop > 30) {
+                this.qNumList = [0, 1, 2]
+                break;
+                //問題の重複回避
+            } else if (this.qNumList.includes(a)) {
+                i--
             } else {
                 this.qNumList[i] = a
             }
 
         }
+
+
     }
 
 })
